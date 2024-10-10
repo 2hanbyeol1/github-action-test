e64014d075d9de42d0d423aff616501123ace6ba
2hanbyeol1 <hblee@searchright.net>
2024-10-10 17:12:07 +0900

    env 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      Hello Github Action!
+    <div className="w-screen h-screen flex justify-center items-center flex-col">
+      <div>Hello Github Action!</div>
+      <div>ENV === [{process.env.VITE_ENV_TEST}]</div>
     </div>
   );
 }
