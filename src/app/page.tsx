c784fec143b32dfd8c20be5e1edb8e3cfa8c1856
2hanbyeol1 <hblee@searchright.net>
2024-10-11 17:13:56 +0900

    build failure test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="w-screen h-screen flex justify-center items-center flex-col">
       <div>Hello Github Action!</div>
-      <div>ENV === [{process.env.VITE_TEST}]</div>
+      <div>ENV === [{process.env.VITE_TEST}]</div
     </div>
   );
 }
